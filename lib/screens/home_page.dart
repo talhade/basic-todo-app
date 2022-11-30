59e6e2f971001c84b7e497e4079e423681270ce7
Talha Demirkan <talhademirkn@gmail.com>
2022-11-30 14:01:23 +0300

    Adding Todo Feature is Added

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:basic_todo_app/models/todo.dart';
 import 'package:basic_todo_app/riverpod/todo_provider.dart';
+import 'package:basic_todo_app/widgets/add_todo_dialog.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -67,78 +68,5 @@ Card todoCard(Todo todo, WidgetRef ref) {
         },
       ),
     ),
-  );
-}
-
-Future<Todo?> todoDialog(
-    BuildContext context,
-    TextEditingController titleController,
-    TextEditingController subtitleController,
-    [Todo? existingTodo]) {
-  String? title = existingTodo?.title;
-  String? subtitle = existingTodo?.subtitle;
-
-  titleController.text = title ?? '';
-  subtitleController.text = subtitle ?? '';
-
-  return showDialog<Todo?>(
-    context: context,
-    builder: (context) {
-      return AlertDialog(
-        title: const Center(child: Text('TODO🖋')),
-        content: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            TextField(
-              controller: titleController,
-              decoration: const InputDecoration(labelText: 'Enter Your Todo:'),
-              onChanged: (value) {
-                title = value;
-              },
-            ),
-            TextField(
-              controller: subtitleController,
-              decoration: const InputDecoration(labelText: 'Enter Description'),
-              onChanged: (value) {
-                subtitle = value;
-              },
-            ),
-          ],
-        ),
-        actions: [
-          TextButton(
-            onPressed: () {
-              Navigator.of(context).pop();
-            },
-            child: const Text('Cancel'),
-          ),
-          ElevatedButton(
-              onPressed: () {
-                //if we have todo:
-                if (title != null && subtitle != null) {
-                  //the todo we have is existing so we are updating:
-                  if (existingTodo != null) {
-                    final newTodo = existingTodo.update(title, subtitle);
-                    Navigator.of(context).pop(newTodo);
-                  }
-                  //the todo we have is not existing so we are creating new
-                  else {
-                    final newTodo = Todo(title: title!, subtitle: subtitle!);
-                    Navigator.of(context).pop(newTodo);
-                  }
-                } else {
-                  Navigator.of(context).pop();
-                }
-              },
-              child: Row(
-                mainAxisSize: MainAxisSize.min,
-                children: const [
-                  Text('Done'),
-                  Icon(Icons.check),
-                ],
-              ))
-        ],
-      );
-    },
   );
 }
